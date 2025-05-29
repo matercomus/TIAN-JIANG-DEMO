760894b9d02e90483c37abb5d67952018af3b820
matercomus <matek.kedzia@gmail.com>
2025-05-29 14:33:59 +0800

    fix writing json error

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ from utils.bili_video import get_all_bvids, fetch_video_info, fetch_uploader_inf
 
 load_dotenv()
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
 
 SESSDATA = os.getenv("SESSDATA")
 BILI_JCT = os.getenv("BILI_JCT")
 BUVID3 = os.getenv("BUVID3")
 TIAN_JIANG_CHANNEL_ID = os.getenv("TIAN_JIANG_CHANNEL_ID")
+
 
 async def main() -> None:
     credential = Credential(sessdata=SESSDATA, bili_jct=BILI_JCT, buvid3=BUVID3)
@@ -25,11 +26,19 @@ async def main() -> None:
     uploader_info = await fetch_uploader_info(user)
     all_video_info = []
     for idx, bvid in enumerate(all_bvids, 1):
-        info = await fetch_video_info(bvid, credential, idx, len(all_bvids), uploader_info)
+        info = await fetch_video_info(
+            bvid, credential, idx, len(all_bvids), uploader_info
+        )
         if info is not None:
             all_video_info.append(info)
-    with open('video_info.json', 'w', encoding='utf-8') as f:
-        json.dump(all_video_info, f, ensure_ascii=False, indent=4)
+    with open("video_info.json", "w", encoding="utf-8") as f:
+        json.dump(
+            [info.model_dump() for info in all_video_info],
+            f,
+            ensure_ascii=False,
+            indent=4,
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
